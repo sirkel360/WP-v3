e8201cebeaa21fa1be7ef32e98215c51b7583486
Yemen Dickinson <48459727+sirkel360@users.noreply.github.com>
2025-05-02 21:08:13 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    strictPort: true,
+    host: '0.0.0.0',
     port: 5173,
-    allowedHosts: 'all',
+    strictPort: true,
+    hmr: {
+      host: process.env.REPL_SLUG + '.' + process.env.REPL_OWNER + '.replit.co',
+      protocol: 'wss',
+    },
+    allowedHosts: ['all'], // still keep this
   },
 });
